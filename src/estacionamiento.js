918a983a3efdf86911fff51efe6d8a29c39dbe59
FullMaury <mauricitogarron@gmail.com>
2025-09-09 10:26:45 -0400

    se implemento el registro de hora de salida

--- src/estacionamiento.js
+++ src/estacionamiento.js
@@ -1,8 +1,19 @@
+// src/estacionamiento.js
+
 let horaIngreso;
+let horaSalida;
 
 function registrarHoraIngreso(hora) {
     horaIngreso = hora;
     return horaIngreso;
 }
 
-module.exports = { registrarHoraIngreso };  
+function registrarHoraSalida(hora) {
+    if (hora < horaIngreso) {
+        throw new Error('La hora de salida no puede ser anterior a la de ingreso');
+    }
+    horaSalida = hora;
+    return horaSalida;
+}
+
+module.exports = { registrarHoraIngreso, registrarHoraSalida };  // Exporta ambas funciones
--- tests/estacionamiento.test.js
+++ tests/estacionamiento.test.js
@@ -1,7 +1,16 @@
 const { registrarHoraIngreso } = require('../src/estacionamiento');
+const { registrarHoraSalida } = require('../src/estacionamiento');
 
 test('Registrar la hora de ingreso', () => {
     const horaIngreso = new Date('2025-09-09T08:00:00');
     const resultado = registrarHoraIngreso(horaIngreso);
     expect(resultado).toEqual(horaIngreso);  
+});
+
+test('Registrar la hora de salida', () => {
+    const horaIngreso = new Date('2025-09-09T08:00:00');
+    const horaSalida = new Date('2025-09-09T10:30:00');
+        registrarHoraIngreso(horaIngreso);  
+        const resultado = registrarHoraSalida(horaSalida);  
+    expect(resultado).toEqual(horaSalida);
 });
